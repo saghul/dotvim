877725a30997577af49ec5d987a6acd97d0b9382
saghul <saghul@gmail.com>
2010-11-25 00:23:48 +0100

    Find the right Ruby binary to compile the extension module

--- install.sh
+++ install.sh
@@ -3,6 +3,18 @@
 NOW=`date +%Y-%m-%d.%H:%M:%S`
 BACKUP_DIR="vim-backup.$NOW.$$"
 
+
+# Find Ruby binary
+RUBY_BIN="NULL"
+for BIN in "ruby" "ruby1.8" "ruby1.9"
+do
+    which $BIN > /dev/null 2>&1
+    if [[ $? -eq 0 ]]; then
+	RUBY_BIN=$BIN
+	break
+    fi
+done
+
 # Backup existing configuration
 mkdir ~/$BACKUP_DIR
 mv ~/.vimrc ~/.vim ~/$BACKUP_DIR
@@ -13,10 +25,12 @@ cp -r * ~/.vim/
 ln -s ~/.vim/vimrc ~/.vimrc
 
 # Rebuild Command-T extension
-pushd ~/.vim/bundle/Command-T/ruby/command-t
-ruby extconf.rb
-make
-popd
+if [[ "$RUBY_BIN" != "NULL" ]]; then
+    pushd ~/.vim/bundle/Command-T/ruby/command-t
+    ruby extconf.rb
+    make
+    popd
+fi
 
 echo "All done!"
 exit 0
